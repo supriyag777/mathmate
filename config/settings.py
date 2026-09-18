cb53e4524f3370435414bfa870334f49fa7f9802
Supriya <supriyahiremath59@gmail.com>
2026-09-18 22:23:13 +0530

    Update ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!6uu&jvnw!sq3@aiid$a)wyybpb6wujjykxjr-0*gkivevsx-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
