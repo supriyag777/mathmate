e56241714ab45ef37e89f397bdcab71dff79bcf3
Supriya <supriyahiremath59@gmail.com>
2026-09-18 23:22:48 +0530

    Fix view function name in solver/urls.py

--- solver/urls.py
+++ solver/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path('', include('solver.urls')),  # Ensure solver.urls is linked to the root path
+    # If your function in views.py is named 'home':
+    path('', views.home, name='home'),
 ]
